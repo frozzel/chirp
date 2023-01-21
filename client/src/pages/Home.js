d199168d2fef504b10373e645f63d6a02d6d9422
Dennis H <111554091+frozzel@users.noreply.github.com>
2023-01-21 12:43:25 -0500

    client server up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import DefaultLayout from '../components/DefaultLayout'
+import {useSelector} from 'react-redux'
+import { Col, Row } from 'antd'
+import Post from '../components/Post'
 function Home() {
-  return (
-    <div>
-      <h1>Home page </h1>
-    </div>
-  )
+    const {users} = useSelector(state=>state.usersReducer)
+    const {posts} = useSelector(state=>state.postsReducer)
+    return (
+        <DefaultLayout>
+              <Row justify='center'>
+                
+                  <Col lg={12} xs={24}>
+
+                      {posts.map(post=>{
+                          return <Post post={post}/>
+                      })}
+                  
+                  </Col>
+
+              </Row>
+        </DefaultLayout>
+    )
 }
 
 export default Home
